415ad6148df25ef30420cb17ad7ded44c3fd636d
mhmdnzr <mohammadnzrhelia@gmail.com>
2024-07-27 12:25:05 +0330

    feat: connect api to idp

--- APIService/src/3.Endpoints/APIService.Endpoints.API/Extentions/HostingExtensions.cs
+++ APIService/src/3.Endpoints/APIService.Endpoints.API/Extentions/HostingExtensions.cs
@@ -1,110 +1,166 @@
 ﻿using APIService.Infra.Data.Sql.Commands.Common;
 using APIService.Infra.Data.Sql.Queries.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+ 
 using Serilog;
+using System.Text;
 using Zamin.EndPoints.Web.Extensions.ModelBinding;
 using Zamin.Extensions.DependencyInjection;
 using Zamin.Infra.Data.Sql.Commands.Interceptors;
+using Zamin.Utilities.SoftwarePartDetector.Options;
 
-namespace APIService.Endpoints.API.Extentions
+namespace APIService.Endpoints.API.Extentions;
+
+public static class HostingExtensions
 {
-    public static class HostingExtensions
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
-        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        IConfiguration configuration = builder.Configuration;
+
+        //zamin
+        builder.Services.AddZaminApiCore("Zamin", "APIService");
+
+        //microsoft
+        builder.Services.AddEndpointsApiExplorer();
+
+        //zamin
+        builder.Services.AddZaminWebUserInfoService(configuration, "WebUserInfo", true);
+
+        //zamin
+        builder.Services.AddZaminParrotTranslator(configuration, "ParrotTranslator");
+
+        //zamin
+        //builder.Services.AddSoftwarePartDetector(configuration, "SoftwarePart");
+
+        //zamin
+        builder.Services.AddNonValidatingValidator();
+
+        //zamin
+        builder.Services.AddZaminMicrosoftSerializer();
+
+        //zamin
+        builder.Services.AddZaminAutoMapperProfiles(configuration, "AutoMapper");
+
+        //zamin
+        builder.Services.AddZaminInMemoryCaching();
+        //builder.Services.AddZaminSqlDistributedCache(configuration, "SqlDistributedCache");
+
+        //CommandDbContext
+        builder.Services.AddDbContext<APIServiceDbContextCommandDbContext>(c => c.UseSqlServer(configuration.GetConnectionString("CommandDb_ConnectionString"))
+            .AddInterceptors(new SetPersianYeKeInterceptor(), new AddAuditDataInterceptor()));
+
+        //QueryDbContext
+        builder.Services.AddDbContext<APIServiceDbContextQueryDbContext>(c => c.UseSqlServer(configuration.GetConnectionString("QueryDb_ConnectionString")));
+
+        //PollingPublisher
+        builder.Services.AddZaminPollingPublisherDalSql(configuration, "PollingPublisherSqlStore");
+        //builder.Services.AddZaminPollingPublisher(configuration, "PollingPublisher");
+
+        //MessageInbox
+        builder.Services.AddZaminMessageInboxDalSql(configuration, "MessageInboxSqlStore");
+        //builder.Services.AddZaminMessageInbox(configuration, "MessageInbox");
+
+        //builder.Services.AddZaminRabbitMqMessageBus(configuration, "RabbitMq");
+
+        //builder.Services.AddZaminTraceJeager(configuration, "OpenTeletmetry");
+
+
+        builder.Services.AddAuthentication("Bearer")
+     .AddJwtBearer("Bearer", options =>
+     {
+         options.Authority = "https://localhost:5001"; // Identity Server URL
+         options.TokenValidationParameters = new TokenValidationParameters
+         {
+             ValidateAudience = false
+         };
+     });
+
+        builder.Services.AddAuthorization(options =>
         {
-            IConfiguration configuration = builder.Configuration;
+            options.AddPolicy("ApiScope", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireClaim("scope", "scope_sapplus");
+            });
+        });
 
-            //zamin
-            builder.Services.AddZaminApiCore("Zamin", "APIService");
-
-            //microsoft
-            builder.Services.AddEndpointsApiExplorer();
-
-            //zamin
-            builder.Services.AddZaminWebUserInfoService(configuration, "WebUserInfo", true);
-
-            //zamin
-            builder.Services.AddZaminParrotTranslator(configuration, "ParrotTranslator");
-
-            //zamin
-            //builder.Services.AddSoftwarePartDetector(configuration, "SoftwarePart");
-
-            //zamin
-            builder.Services.AddNonValidatingValidator();
-
-            //zamin
-            builder.Services.AddZaminMicrosoftSerializer();
-
-            //zamin
-            builder.Services.AddZaminAutoMapperProfiles(configuration, "AutoMapper");
-
-            //zamin
-            builder.Services.AddZaminInMemoryCaching();
-            //builder.Services.AddZaminSqlDistributedCache(configuration, "SqlDistributedCache");
-
-            //CommandDbContext
-            builder.Services.AddDbContext<APIServiceDbContextCommandDbContext>(c => c.UseSqlServer(configuration.GetConnectionString("CommandDb_ConnectionString"))
-                .AddInterceptors(new SetPersianYeKeInterceptor(), new AddAuditDataInterceptor()));
-
-            //QueryDbContext
-            builder.Services.AddDbContext<APIServiceDbContextQueryDbContext>(c => c.UseSqlServer(configuration.GetConnectionString("QueryDb_ConnectionString")));
-
-            //PollingPublisher
-            builder.Services.AddZaminPollingPublisherDalSql(configuration, "PollingPublisherSqlStore");
-            //builder.Services.AddZaminPollingPublisher(configuration, "PollingPublisher");
-
-            //MessageInbox
-            builder.Services.AddZaminMessageInboxDalSql(configuration, "MessageInboxSqlStore");
-            //builder.Services.AddZaminMessageInbox(configuration, "MessageInbox");
-
-            //builder.Services.AddZaminRabbitMqMessageBus(configuration, "RabbitMq");
-
-            //builder.Services.AddZaminTraceJeager(configuration, "OpenTeletmetry");
-
-            builder.Services.AddSwaggerGen();
-
-            return builder.Build();
-        }
-
-        public static WebApplication ConfigurePipeline(this WebApplication app)
+        builder.Services.AddSwaggerGen(c =>
         {
-            //zamin
-            app.UseZaminApiExceptionHandler();
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "SapPlus CompanyAPI", Version = "v1" });
 
-            //Serilog
-            app.UseSerilogRequestLogging();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            // Add security definition
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseStatusCodePages();
-
-            app.UseCors(delegate (CorsPolicyBuilder builder)
-            {
-                builder.AllowAnyOrigin();
-                builder.AllowAnyHeader();
-                builder.AllowAnyMethod();
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme."
             });
 
-            app.UseHttpsRedirection();
+            // Add security requirement
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
 
-            //app.Services.ReceiveEventFromRabbitMqMessageBus(new KeyValuePair<string, string>("MiniAggregateName", "AggregateNameCreated"));
+        return builder.Build();
+    }
 
-            //var useIdentityServer = app.UseIdentityServer("OAuth");
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        //zamin
+        app.UseZaminApiExceptionHandler();
 
-            var controllerBuilder = app.MapControllers();
+        //Serilog
+        app.UseSerilogRequestLogging();
 
-            //if (useIdentityServer)
-            //    controllerBuilder.RequireAuthorization();
-
-            //app.Services.GetService<SoftwarePartDetectorService>()?.Run();
-
-            return app;
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseStatusCodePages();
+
+        app.UseCors(delegate (CorsPolicyBuilder builder)
+        {
+            builder.AllowAnyOrigin();
+            builder.AllowAnyHeader();
+            builder.AllowAnyMethod();
+        });
+
+        app.UseHttpsRedirection();
+        //app.Services.ReceiveEventFromRabbitMqMessageBus(new KeyValuePair<string, string>("MiniAggregateName", "AggregateNameCreated"));
+
+        //var useIdentityServer = app.UseIdentityServer("OAuth");
+        app.UseAuthentication();
+        app.UseAuthorization();
+        var controllerBuilder = app.MapControllers().RequireAuthorization("ApiScope");
+
+        //if (useIdentityServer)
+        //    controllerBuilder.RequireAuthorization();
+
+        //app.Services.GetService<SoftwarePartDetectorService>()?.Run();
+
+        return app;
     }
 }
